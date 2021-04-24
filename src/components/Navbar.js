269011612e1af7428fe62e3d54f51c9250ccd7bd
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2021-04-24 18:37:56 +0100

    working on the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,26 +7,6 @@ import {Link} from "react-router-dom"
 function Navbar() {
     return (
         <div>
-            <header className="hero">
-                <nav className="navbar">
-                    <h1 className="logo">
-                        <Link to="/">QUINCY OGHENETEJIRI</Link>
-                    </h1>
-                    <div>
-                        <ul className="nav-menu">
-                            <li className="nav-item"><Link to="/" className="nav-link">Home</Link></li>
-                            <li className="nav-item"><Link to="" className="nav-link">About</Link></li>
-                            <li className="nav-item"><Link to="" className="nav-link">Contact</Link></li>
-                            <li className="nav-item"><Link to="" className="nav-link">Services</Link></li>
-                        </ul>
-                        <div className="hamburger">
-                            <span className="bar"></span>
-                            <span className="bar"></span>
-                            <span className="bar"></span>
-                        </div>
-                    </div>
-                </nav>
-        </header> 
         </div>
     )
 }
