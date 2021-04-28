4a27b42aa640ce7ef19154c5534abfba80453111
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2021-04-28 12:02:24 +0100

    working

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import "./css/About.css"
-import {Skills} from "./Skill"
 
 
 class About extends Component {
@@ -16,18 +15,8 @@ class About extends Component {
                         <div className="col-sm-6 col-sm-6 col-xl-6">
                             <h2 className="about-border">About me & What I do </h2>
                             <p>i'm Ukumakube Quincy Oghenetejiri, a full stack developer with over one year experience. I design and develop web applications and microservices using figma for design and mostly Javascript and Python for development . My most notable stacks include :
-                                <img src="https://res.cloudinary.com/dha7gjz6y/image/upload/v1619606586/All_Skills_difax1.png" />
-                                {Skills.map((item,index)=>{
-                                    return (
-                                        <div key={index} className={item.cName}>
-                                            {item.skill}
-                                        </div>
-                                    )
-                                })}
-
-
-
-                                
+                                <img className="desktop" src="https://res.cloudinary.com/dha7gjz6y/image/upload/v1619606984/All_Skills_1_ndpkfm.png" />
+                                <img className="mobile" src="https://res.cloudinary.com/dha7gjz6y/image/upload/v1619606996/All_Skills_2_pp7yxs.png" />
                             </p>
 
                         </div>
