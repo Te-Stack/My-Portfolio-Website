d27d222d2b2079a7dfef87bf5d4a927f811770c4
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2021-04-28 20:03:43 +0100

    working

--- src/components/About.js
+++ src/components/About.js
@@ -10,13 +10,13 @@ class About extends Component {
                     <div className="row">
                         <div className="col-sm-6 col-sm-6 col-xl-6">
                             <img className="img" src="https://res.cloudinary.com/dha7gjz6y/image/upload/v1619389551/20200830_104248_emf6t0.jpg" alt="Quincy-Oghentejiri;s pics " loading="lazy"/>
-                            <button>Download CV</button>
+                            <button className="btn mt-1 mb-3 mx-0">Download CV</button>
                         </div>
                         <div className="col-sm-6 col-sm-6 col-xl-6">
                             <h2 className="about-border">About me & What I do </h2>
                             <p>i'm Ukumakube Quincy Oghenetejiri, a full stack developer with over one year experience. I design and develop web applications and microservices using figma for design and mostly Javascript and Python for development . My most notable stacks include :
-                                <img className="desktop" src="https://res.cloudinary.com/dha7gjz6y/image/upload/v1619606984/All_Skills_1_ndpkfm.png" />
-                                <img className="mobile" src="https://res.cloudinary.com/dha7gjz6y/image/upload/v1619606996/All_Skills_2_pp7yxs.png" />
+                                <img alt="Skills" className="desktop" src="https://res.cloudinary.com/dha7gjz6y/image/upload/v1619606984/All_Skills_1_ndpkfm.png" />
+                                <img alt="Skills" className="mobile" src="https://res.cloudinary.com/dha7gjz6y/image/upload/v1619606996/All_Skills_2_pp7yxs.png" />
                             </p>
 
                         </div>
--- src/components/Project.js
+++ src/components/Project.js
@@ -5,7 +5,31 @@ class Project extends Component {
     render() {
         return (
             <div className="Projects">
-                Projects
+                <div className="container">
+                    <div className="row1">
+                        <div className=" cards">
+                            <h5>Balance Sheet App</h5>
+                        </div>
+                        <div className=" cards">
+                            <h5>Balance Sheet App</h5>
+                        </div>
+                        <div className=" cards">
+                            <h5>Balance Sheet App</h5>
+                        </div>
+                    </div>
+
+                    <div className="row2 mt-5">
+                        <div className=" cards">
+                            <h5>Balance Sheet App</h5>
+                        </div>
+                        <div className=" cards">
+                            <h5>Balance Sheet App</h5>
+                        </div>
+                        <div className="cards">
+                            <h5>Balance Sheet App</h5>
+                        </div>
+                    </div>
+                </div>
                 
             </div>
         )
