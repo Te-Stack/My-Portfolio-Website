9e6cb09762e13c4762dc578e18e07108ceb7a689
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2021-04-28 11:44:56 +0100

    working

---
+++ src/components/Skill.js
@@ -0,0 +1,36 @@
+export const Skills = [
+    {
+        skill:"#HTML",
+        cName: "Skill"
+    },
+    {
+        skill:"#CSS",
+        cName: "Skill"
+    },
+    {
+        skill:"#Bootstrap",
+        cName: "Skill"
+    },
+    {
+        skill:"#Python",
+        cName: "Skill"
+    },
+    {
+        skill:"#Jquery",
+        cName: "Skill"
+    },
+    {
+        skill:"#Axios",
+        cName: "Skill"
+    },
+    
+    {
+        skill:"#JavaScript",
+        cName: "Skill"
+    },
+    {
+        skill:"#Express.js",
+        cName: "Skill"
+    },
+    
+]
