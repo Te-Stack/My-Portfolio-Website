b337ad54ed93135bd0f93b7bd029db670eb821a4
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2021-04-29 22:57:00 +0100

    working

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Contact from "./components/Contact"
 import Project from "./components/Project"
 import Home from './components/Home';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 
 function App() {
@@ -22,6 +23,7 @@ function App() {
       <Route path='/' exact component = {Project} />
       <Route path='/' exact component = {Contact} />
       </Switch>
+      <Footer/>
       </Router>
     </div>
   )
