91be5094287a34247b7bb4bcb99806c73747e550
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2021-04-29 23:01:50 +0100

    working

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react'
 class Footer extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="footer">
+                <h1 className="navbar-logo">Quincy Oghenetejiri</h1> 
             </div>
         )
     }
