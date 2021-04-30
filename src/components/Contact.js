d5c1f776f67ba48ca9113f3cff6396698b0a01b3
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2021-04-30 21:38:14 +0100

    working

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,9 +5,31 @@ class Contact extends Component {
     render() {
         return (
             <div className="Contact">
-                <h2 className="project-border">Projects</h2>
-                
-                
+                <h2 className="contact-border">Contact Me</h2>
+                <p>Thinking of ways of converting your ideas into reality through designs and development. Reach out to me</p>
+                <div className ="container">
+                    <div className="row">
+                    <div className="col-sm-6 col-sm-6 col-xl-6">
+                        <p><a>ukumakubequincy@gmail.com</a></p>
+                        <p><a>+2349027499683</a></p>
+                        <p><a>Benin,Nigeria</a></p>
+                    </div>
+                    <div className="col-sm-6 col-sm-6 col-xl-6">
+                        <h6 className="send-border">Send me a message</h6>
+                        <form>
+                            <input type="text" placeholder="Name" name="Name" className="text-center" />
+                            <input type="email" placeholder="Email" name="email" className="mx-2 text-center" />
+                            <input type="text" placeholder="Phone Number" name="Phone Number" className="m-2 mx-0 text-center" />
+                            <textarea placeholder="Enter Your Message" name="Message" className="text-center" >
+                            </textarea>
+                            <button type="submit" className="btn">Submit</button>
+
+                        </form>
+
+                    </div>
+                        
+                    </div>
+                </div>   
             </div>
         )
     }
