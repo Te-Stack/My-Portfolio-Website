cbf893174d067721fdb32e28500483ead1902899
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2021-05-01 11:09:31 +0100

    working

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,8 @@ import "./css/Footer.css"
 class Footer extends Component {
     render() {
         return (
-            <div className="footer">
+            <div>
+
             </div>
         )
     }
