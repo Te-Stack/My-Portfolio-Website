cd52d250be08df69bf998814c897b4e0c7dad0c9
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2021-05-03 12:25:33 +0100

    adding links

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,7 +7,7 @@ class Contact extends Component {
             <div className="Contact">
                 <h2 className="contact-border">Contact Me</h2>
                 <p className="Think">Thinking of ways of converting your ideas into reality through designs and development. Reach out to me</p>
-                <div className ="container">
+                <div className ="container-fluid">
                     <div className="row">
                     <div className="col-sm-6 col-sm-6 col-xl-6">
                         <p className="reach"><a href="https://mailto:ukumakubequincy@gmail.com"><i className="fas fa-envelope alt"></i>ukumakubequincy@gmail.com</a></p>
@@ -18,7 +18,7 @@ class Contact extends Component {
                         <h6 className="send-border">Send me a message</h6>
                         <form>
                             <input type="text" placeholder="Name" name="Name" className="text-center" />
-                            <input type="email" placeholder="Email" name="email" className="mx-2 text-center" />
+                            <input type="email" placeholder="Email" name="email" className="text-center" />
                             <input type="text" placeholder="Phone Number" name="Phone Number" className="m-2 mx-0 text-center" />
                             <textarea placeholder="Enter Your Message" name="Message" className="text-center" >
                             </textarea>
