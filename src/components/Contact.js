f71a2bb37621abcefb4ce5fcbc536c8c346b5e06
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2021-08-11 11:49:09 +0100

    Working

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,10 +12,10 @@ function Contact() {
                 <h2 className="contact-border">Contact Me</h2>
                 <p className="Think">Thinking of ways of converting your ideas into reality through designs and development. Reach out to me</p>
                 <div className ="container-fluid">
-                    <div className="row">
+                    <div className="row move">
                     <div className="col-sm-6 col-md-6 col-xl-6 ml-2">
                         <p className="reach"><a href="https://mailto:ukumakubequincy@gmail.com"><i className="fas fa-envelope alt"></i>ukumakubequincy@gmail.com</a></p>
-                        <p className="reach"><a href="https://google.com"><i className="fas fa-phone-alt alt"></i>+2349027499683</a></p>
+                        <p className="reach"><a href="tel:+2349027499683"><i className="fas fa-phone-alt alt"></i>+2349027499683</a></p>
                         <p className="reach"><a href="https://google.com"><i className="fas fa-map-marker-alt alt"></i>Benin,Nigeria</a></p>
                     </div>
                     <div className="col-sm-6 col-sm-6 col-xl-6 animate__animated animate__zoomIn animate__delay-3s">
